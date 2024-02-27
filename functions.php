bd1007b0c73aa9dec3fed930a75b050667e6faf3
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-27 18:49:37 +0100

    creazione layout e conclusione milestone 1 e 2

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function genRandomChar(){
+    $out = "";
+    $charSel = rand(1,4);
+    switch ($charSel){
+        case 1:
+            $out = chr(rand(48,57));
+            break;
+        case 2:
+            $out = chr(rand(65,90));
+            break;
+        case 3:
+            $out = chr(rand(97,122));
+            break;
+        case 4:
+            $out = chr(rand(35,38));
+            break;
+        default:
+            $out = "";
+    }
+    return $out;
+}
+
+function genPW($leng){
+    $generatedPW = "";
+    for($i=0; $i<$leng; $i++){
+        $generatedPW .= genRandomChar();
+    }
+    echo $generatedPW;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+$passwordLenght = $_GET["pwdlen"] ?? 0;
+
+include './functions.php'
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PWD Gen</title>
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+</head>
+<body>
+    <div id="back">
+
+        <h1 class="text-center">Strong Password Generator</h1>
+        <h2 class="text-center">Genera una password sicura</h2>
+        <div class="text-center p-5">
+            <?php if($passwordLenght != 0) : ?>
+                <h4> <?=  genPW($passwordLenght)  ?> </h4>
+            <?php endif ?>
+            <form action="index.php" method="get">
+                <label for="pwdlen">Lunghezza password: </label>
+                <input type="number" name="pwdlen" id="pwdlen" min="1">
+                <button type="submit" class="btn btn-primary">Genera</button>
+            </form>
+        </div>
+
+    </div>
+
+
+
+<style lang="scss">
+    #back{
+        background-color: #001632;
+        height: 100vh;
+        color: lightcyan;
+        padding: 20px 15%;
+    }
+    div{
+        color: black;
+        background-color: white;
+    }
+</style>
+    
+</body>
+</html>
